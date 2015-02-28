b6f41f808f07059719ca5dc6b06eafa57c482533
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-27 19:37:10 -0500

    backtrack: different approach needed

--- gotypeset.go
+++ gotypeset.go
@@ -1,59 +1,16 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
-	"os"
 	"os/exec"
 )
 
-var multimarkdownBin string
+var mmd2pdfBin string
 
 func init() {
-	mmd, err := exec.LookPath("multimarkdown")
+	mmd, err := exec.LookPath("mmd2pdf")
 	if err != nil {
-		log.Fatal("multimarkdown needs to be installed")
+		log.Fatal("multimarkdown (and mmd2pdf) need to be installed")
 	}
-	multimarkdownBin = mmd
-}
-
-func mmd2tex(in io.Reader) (io.Reader, error) {
-	cmd := exec.Command(multimarkdownBin, "--to=latex")
-	cmd.Stdin = in
-	out, err := cmd.Output()
-	if err != nil {
-		return nil, err
-	}
-	return bytes.NewReader(out), nil
-}
-
-func main() {
-	inputPaths := os.Args[1:]
-	if len(inputPaths) < 1 {
-		log.Fatal("Must provide the path of at least one file to convert")
-	}
-
-	contents := make([][]byte, 0, 5)
-	for _, p := range inputPaths {
-		c, err := ioutil.ReadFile(p)
-		if err != nil {
-			log.Fatal("Could not read file at " + p)
-		}
-		contents = append(contents, c)
-	}
-
-	in := bytes.NewReader(bytes.Join(contents, []byte("\n\n")))
-	out, err := mmd2tex(in)
-	if err != nil {
-		log.Fatal("Could not convert input to LaTeX")
-	}
-	output, err := ioutil.ReadAll(out)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(output))
+	mmd2pdfBin = mmd
 }
